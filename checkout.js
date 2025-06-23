9ae062205f06339291abdfb020051ffc53edb669
TrungQuyen-TQ <nq5663558@gmail.com>
2025-06-23 14:36:30 +0700

    loi o main

--- checkout.js
+++ checkout.js
@@ -1,1 +1,11 @@
 "// checkout module buggy" 
+function checkout() {
+    return false; // ❌ BUG: trả về sai
+  }
+  
+  if (!checkout()) {
+    console.log("❌ Thanh toán thất bại");
+  } else {
+    console.log("✅ Thanh toán thành công");
+  }
+  
